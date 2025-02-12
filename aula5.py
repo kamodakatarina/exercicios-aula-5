2f2f2d7c63fa1ecf491c5efa1117881e3a652c34
kamodakatarina <kamoda.katarina@gmail.com>
2025-02-12 15:31:14 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,48 @@
+
+print('Sejam bem vindos')
+
+booleana =  True
+tipo = type(booleana)
+print(tipo)
+
+multi =  (5.5 * 4.9) + 20 
+print(multi)
+
+div = 10/2
+print(div)
+
+print(10-2)
+
+print(10/2)
+
+numeros  =  4* 2* 3 * 2
+print(numeros)
+
+numero_int = 10
+print(numero_int ** 2)
+
+n1 = float(input('Digite um número:'))
+n2 = float(input('Digite um número:'))
+
+soma =  n1 + n2
+subtracao = n1 -n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print(f'soma: {soma} | subtração: {subtracao} | multiplicação: {multiplicacao} | divisão {divisao}   ')
+
+
+nome  =  input('Digite seu nome: ')
+idade  =  int(input('Digite sua idade: '))
+email =  input('Digite seu e-mail: ')
+endereco = input('Digite seu endereço')
+
+print('..........................................')
+print('DADOS CADASTRADOS:')
+
+print(f''' 
+NOME: {nome}
+IDADE: {idade}
+E-MAIL:{email}
+ENDEREÇO: {endereco}
+''')
